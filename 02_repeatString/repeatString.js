c783228d0e22efcc1c21f7d3196e1e15be7c9db6
scarlytwastaken <173748550+scarlytwastaken@users.noreply.github.com>
2024-07-03 01:12:55 -0700

    Complete and test exercises 1, 2, and 3

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
 
+
+const repeatString = function(stringToRepeat, numRepeats) {
+    repeatedString = ''
+    if (numRepeats < 0) {
+        return 'ERROR';
+    }
+    for (let i = 0; i < numRepeats; ++i) {
+        repeatedString += stringToRepeat;
+    }
+    return repeatedString;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function reverseString(input) {
+    inputString = input
+    inputString = inputString.split('');
+    inputString = inputString.reverse();
+    inputString = inputString.join('');
+    return inputString;
 };
+
+reverseString('hello');
 
 // Do not edit below this line
 module.exports = reverseString;
