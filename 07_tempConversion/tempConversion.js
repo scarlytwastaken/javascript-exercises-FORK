0cc9a8037ee015611f6df3b3622b61cc9ffe156a
scarlytwastaken <173748550+scarlytwastaken@users.noreply.github.com>
2024-07-05 16:08:46 -0700

    Exercise 07 code test test test

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+
+  inC = ((f - 32) * (5 / 9));
+  return Math.round(inC * 10) / 10;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+
+  inF = (c * (9 / 5) + 32);
+  return Math.round(inF * 10) / 10;
+
 };
 
 // Do not edit below this line
