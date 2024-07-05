79df05865a2a5d51b384da78b8c4d1c5385ddc88
scarlytwastaken <173748550+scarlytwastaken@users.noreply.github.com>
2024-07-05 05:05:42 -0700

    Exercise 4 write, test, test, test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(fullArray, ...allArgs) {
 
+    while (allArgs.length > 0) {
+        argArray = allArgs;
+        eachArg = argArray.shift();
+        
+        while (fullArray.indexOf(eachArg) !== -1) {
+            argIndex = fullArray.indexOf(eachArg);
+            fullArray.splice(argIndex, 1);
+        }    
+                
+    }
+
+    return fullArray;
+
+ };
 // Do not edit below this line
 module.exports = removeFromArray;
