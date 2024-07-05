1c4821eafac1a3bbdb02c16872ced33cb5a90384
scarlytwastaken <173748550+scarlytwastaken@users.noreply.github.com>
2024-07-05 05:33:58 -0700

    Exercise 05 code test test test test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,31 @@
-const sumAll = function() {
+const sumAll = function(first,second) {
+    let firstInt = first;
+    let secondInt = second;
+    let sumOfAll = firstInt;
 
+    if (first < 0 || second < 0 || typeof(first) != 'number' || typeof(second) != 'number') {
+        return 'ERROR';
+    }
+   
+    if (first<second) {
+        while (firstInt < secondInt) {
+            firstInt += 1;
+            sumOfAll += firstInt;
+            console.log(sumOfAll);
+        }
+    }
+
+    if (first>second) {
+        while (firstInt > secondInt) {
+            firstInt -= 1;
+            sumOfAll += firstInt;
+            console.log(sumOfAll);
+        }
+    }
+
+
+    return sumOfAll
+    
 };
 
 // Do not edit below this line
